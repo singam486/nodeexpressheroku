d05c3280afaff7581d417b397a8e82a2c2779acd
Singam Ramasubbareddy <singam.ramasubbareddy@ibm.com>
2021-06-23 20:38:29 +0530

    added gitignore

--- expressdemo.js
+++ expressdemo.js
@@ -2,9 +2,12 @@ const express = require('express')
 
 const app = express()
 
+var port = process.env.PORT || 9090
+
+// http://localhost:9090/ibm
 app.get('/',function(req,res){
     res.send('<h2>Welocome To Express Server </h2>')
 })
-app.listen(9090,function(){
+app.listen(port,function(){
     console.log('express server started')
 })
